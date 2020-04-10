fdf2c267e17cdc9b36fb54a84320d46f66ae4d8d
sharma1612harshit <sharma1612harshit@gmail.com>
2020-04-10 22:44:18 +0530

    force in terraform destroy destroy.go:26

--- modules/terraform/destroy.go
+++ modules/terraform/destroy.go
@@ -1,7 +1,6 @@
 package terraform
 
 import (
-	"flag"
 	"github.com/gruntwork-io/terratest/modules/testing"
 	"github.com/stretchr/testify/require"
 )
@@ -22,8 +21,7 @@ func TgDestroyAll(t testing.TestingT, options *Options) string {
 
 // DestroyE runs terraform destroy with the given options and return stdout/stderr.
 func DestroyE(t testing.TestingT, options *Options) (string, error) {
-	flag.Args()
-	return RunTerraformCommandE(t, options, FormatArgs(options, "destroy", "-input=false", "-auto-approve")...)
+	return RunTerraformCommandE(t, options, FormatArgs(options, "destroy","-force", "-input=false", "-auto-approve")...)
 }
 
 // TgDestroyAllE runs terragrunt destroy with the given options and return stdout.
