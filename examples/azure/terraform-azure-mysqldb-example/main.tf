6ea41829b9e527c9f34b6423887a896a352d5370
Dave Seepersad <68443470+davesee@users.noreply.github.com>
2021-01-09 10:57:30 -0500

    Enforce complex password requirement mysqldb

--- examples/azure/terraform-azure-mysqldb-example/main.tf
+++ examples/azure/terraform-azure-mysqldb-example/main.tf
@@ -33,6 +33,10 @@ resource "random_password" "password" {
   length           = 16
   special          = true
   override_special = "_%@"
+  min_upper        = "1"
+  min_lower        = "1"
+  min_numeric      = "1"
+  min_special      = "1"
 }
 
 resource "azurerm_mysql_server" "mysqlserver" {
