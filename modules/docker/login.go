bbe53e824fd4a621b88b8c5419aadc13cf323df0
Maksim Sokolnikov <stuffsweep@gmail.com>
2024-11-21 17:41:14 +0300

    Fix MR

--- modules/docker/login.go
+++ modules/docker/login.go
@@ -16,12 +16,13 @@ type LoginOptions struct {
 // Login runs the 'docker login' command to login the given registry. This will fail the test if there are any errors.
 // Do not use production password, only for testing purpose.
 func Login(t testing.TestingT, opt LoginOptions) {
-	require.NoError(t, login(t, opt))
+	require.NoError(t, LoginE(t, opt))
 }
 
 // login runs the 'docker login' command to login the given registry.
-func login(t testing.TestingT, opt LoginOptions) error {
+func LoginE(t testing.TestingT, opt LoginOptions) error {
 	logger.Logf(t, "Running 'docker login' for user %s", opt.Login)
+
 	cmd := shell.Command{
 		Command: "docker",
 		Args:    []string{"login", opt.Registry, "-u", opt.Login, "-p", opt.Password},
