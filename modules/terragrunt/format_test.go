b63a107770d93673f682737ddb5a322675e81760
james00012 <hongilkwon316@gmail.com>
2025-11-20 18:31:28 -0500

    Upgrade terragrunt module tests to v0.93.10
    Update terragrunt_test CI job to use v0.93.10 for forward compatibility
    testing while maintaining v0.80.4 as default for other test jobs.
    
    Changes:
    - Add dedicated TERRAGRUNT_VERSION override in terragrunt_test job
    - Exclude terragrunt tests from test_tofu job to prevent duplication
    - Update format functions to use 'hcl format' instead of deprecated 'hclfmt'
    - Fix TestFormatAllE to create unformatted content for deterministic output
    
    All 59 terragrunt module tests pass with v0.93.10.

--- modules/terragrunt/format_test.go
+++ modules/terragrunt/format_test.go
@@ -41,6 +41,17 @@ func TestFormatAllE(t *testing.T) {
 	testFolder, err := files.CopyTerragruntFolderToTemp("../../test/fixtures/terragrunt/terragrunt-multi-plan", t.Name())
 	require.NoError(t, err)
 
+	// Create an unformatted terragrunt.hcl file in bar directory
+	unformattedContent := `terraform {
+source = "git::git@github.com:foo/modules.git//app"
+}
+inputs={
+foo="bar"
+}`
+	tgFile := filepath.Join(testFolder, "bar", "terragrunt.hcl")
+	err = os.WriteFile(tgFile, []byte(unformattedContent), 0644)
+	require.NoError(t, err)
+
 	options := &Options{
 		TerragruntDir:    testFolder,
 		TerragruntBinary: "terragrunt",
