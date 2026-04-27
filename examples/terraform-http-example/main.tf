02cd5b91cc122b4e0802ea7024558ce102fe65eb
james00012 <hongilkwon316@gmail.com>
2026-04-26 20:51:28 -0400

    chore(examples): bump Terraform required_version to >= 1.0, refresh provider pins
    Updated all examples/ to target modern Terraform/OpenTofu (>= 1.0)
    and current AWS/Azure/GCP provider versions. Removed stale 2020-era
    comments referencing 0.12.x/0.13.x pins.
    
    Linear: OSS-3399

--- examples/terraform-http-example/main.tf
+++ examples/terraform-http-example/main.tf
@@ -1,13 +1,12 @@
-# ---------------------------------------------------------------------------------------------------------------------
-# PIN TERRAFORM VERSION TO >= 0.12
-# The examples have been upgraded to 0.12 syntax
-# ---------------------------------------------------------------------------------------------------------------------
-
 terraform {
-  # This module is now only being tested with Terraform 0.13.x. However, to make upgrading easier, we are setting
-  # 0.12.26 as the minimum version, as that version added support for required_providers with source URLs, making it
-  # forwards compatible with 0.13.x code.
-  required_version = ">= 0.12.26"
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -26,7 +25,10 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
-  user_data              = data.template_file.user_data.rendered
+  user_data = templatefile("${path.module}/user-data/user-data.sh", {
+    instance_text = var.instance_text
+    instance_port = var.instance_port
+  })
   vpc_security_group_ids = [aws_security_group.example.id]
 
   tags = {
@@ -49,19 +51,6 @@ resource "aws_security_group" "example" {
     # To keep this example simple, we allow incoming HTTP requests from any IP. In real-world usage, you may want to
     # lock this down to just the IPs of trusted servers (e.g., of a load balancer).
     cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# ---------------------------------------------------------------------------------------------------------------------
-# CREATE THE USER DATA SCRIPT THAT WILL RUN DURING BOOT ON THE EC2 INSTANCE
-# ---------------------------------------------------------------------------------------------------------------------
-
-data "template_file" "user_data" {
-  template = file("${path.module}/user-data/user-data.sh")
-
-  vars = {
-    instance_text = var.instance_text
-    instance_port = var.instance_port
   }
 }
 
